34a9d5c2c325481121c4a67a8436004edd95801b
Joel Ross <joelross@uw.edu>
2023-10-24 05:41:10 -0700

    Add functional JavaScript demo statrer code

---
+++ javascript-functional/js/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const peopleArray = [
+  {name: 'Ada', height: 64, weight: 135},
+  {name: 'Bob', height: 74, weight: 156, pronoun: 'they/them'},
+  {name: 'Chris', height: 69, weight: 139},
+  {name: 'Diya', height: 69, weight: 144},
+  {name: 'Emma', height: 71, weight: 152}
+]
+console.log(peopleArray);
