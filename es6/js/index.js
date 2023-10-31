08fcb481e7b219e0c36267ce6328214d8601a1c4
Joel Ross <joelross@uw.edu>
2023-10-31 09:32:17 -0700

    Add ES6 demo starter code

---
+++ es6/js/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const monsterArray = [
+  {name: 'Dracula', type: 'vampire', height: 64},
+  {name: 'Frankenstein', type: 'zombie', height: 74},
+  {name: 'Mr. Hyde', type: 'mad scientist', height: 69},
+  {name: 'Mummy', type: 'mummy', height: 69},
+  {name: 'Creature from the Black Lagoon', type: 'fish', height: 71}
+]
+console.log(monsterArray);
